bacd43cc5e33b453689771d2bb5a1e5761f13555
jackr276 <jack_m_robbins@me.com>
2023-11-25 11:06:40 -0500

    began work on parserInterp

--- parserInterp.cpp
+++ parserInterp.cpp
@@ -4,3 +4,133 @@
 */
 
 #include "parserInterp.h"
+#include <iostream>
+#include <set>
+
+// defVar keeps track of all variables that have been defined in the program thus far
+map<string, bool> defVar;
+// SymTable keeps track of the type for all of our variables
+map<string, Token> SymTable;
+//Container of temporary locations of Value objects for results of expressions, variables values and constants 
+//Key is a variable name, value is its Value. Holds all variables defined by assign statements
+map<string, Value> TempsResults;
+//declare a pointer variable to a queue of Value objects
+queue <Value> * ValQue;
+
+
+//The parser namespace that interacts with lex for us
+namespace Parser {
+	bool pushed_back = false;
+	LexItem	pushed_token;
+
+	static LexItem GetNextToken(istream& in, int& line) {
+		if( pushed_back ) {
+			pushed_back = false;
+			return pushed_token;
+		}
+		return getNextToken(in, line);
+	}
+
+	static void PushBackToken(LexItem & t) {
+		if( pushed_back ) {
+			abort();
+		}
+		pushed_back = true;
+		pushed_token = t;	
+	}
+}
+
+//Initialize error count to be 0
+static int error_count = 0;
+
+
+// A simple wrapper that allows access to the number of syntax errors
+int ErrCount(){
+    return error_count;
+}
+
+
+//A simple error wrapper that incrememnts error count, and prints out the error
+void ParseError(int line, string msg)
+{
+	++error_count;
+	cout << line << ": " << msg << endl;
+}
+
+
+/**
+ * Prog is the entry point to our entire interpreter, the "root" of our parse tree
+ * To start, the program must use the keyword Program and give an identifier name.
+ * It must then go into the Declaritive part followed by a compound statement
+ * Prog ::= PROGRAM IDENT ; DeclPart CompoundStmt
+*/
+bool Prog(istream& in, int& line){
+	bool status = false;
+
+	//This should be the keyword "program"
+	LexItem l = Parser::GetNextToken(in, line);
+
+	//We're missing the required program keyword, throw an error and exit
+	if (l != PROGRAM){
+		ParseError(line, "Missing PROGRAM keyword.");
+		return false;
+	} else {
+		//We have the program keyword, move on to more processing
+		l = Parser::GetNextToken(in, line);
+
+		//This token should be an IDENT if all is correct, if not we have an error
+		if (l != IDENT){
+			ParseError(line, "Missing Program name.");
+			return false;
+		}
+
+		//If we're at this point we have PROGRAM IDENT, need a semicol
+		l = Parser::GetNextToken(in, line);
+
+		//If there's no semicolon, syntax error
+		if (l != SEMICOL) {
+			ParseError(line, "Syntax Error.");
+			return false;
+		}
+
+		//By this point, we have checked up to PROGRAM IDENT ;
+		//Check the DeclPart
+		status = DeclPart(in, line);
+		
+		//If the declaration was bad, no point in continuing
+		if (!status){
+			ParseError(line, "Incorrect Declaration Section.");
+			return false;
+		}
+
+		//Up to here we have gotten PROGRAM IDENT ; DeclPart
+		//Check for the compound statement, make sure that there actually is a BEGIN
+		l = Parser::GetNextToken(in, line);
+		if (l != BEGIN){
+			ParseError(line, "Syntactic Error in Declaration Block.");
+			ParseError(line, "Incorrect Declaration Section");
+			return false;
+		}
+
+		//If we  have BEGIN, consume it and call CompoundStmt
+		status = CompoundStmt(in, line);
+
+		//If the compound statement was bad, return false
+		if (!status){
+			ParseError(line, "Incorrect Program Body.");
+			return false;
+		}
+
+	}
+
+	//There could also be some unrecognizable token here
+	if (l == ERR) {
+		ParseError(line, "Unrecognized input pattern.");
+		cout << "(" << l.GetLexeme() << ")" << endl;
+		return false; 
+	}
+
+	//If we reach here, status will be true and parsing will have been successful
+	return status;
+}
+
